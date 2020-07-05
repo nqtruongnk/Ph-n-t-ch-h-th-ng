0f6412c483ecc65fded61a17ba004a8a2bc6b91f
nqtruongnk <54401331+nqtruongnk@users.noreply.github.com>
2020-07-05 20:46:25 +0700

    login

---
+++ Phantichthietkehethong/WindowsFormsApp1/login he thong.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class login_he_thong : Form
+    {
+        public login_he_thong()
+        {
+            InitializeComponent();
+        }
+    }
+}
